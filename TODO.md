8ff9f658628aa32ca05144322e22ca4d9f50739e
Luke <lukedesmondjames@gmail.com>
2024-08-14 18:50:26 +0100

    review widget css complete

--- TODO.md
+++ TODO.md
@@ -4,6 +4,5 @@ Css for product page:
 - sticky header
 
 - Add to cart functionality
-- reviews widget css
 - Product collection grid + react router
 - Start building other pages: menu, home, collections, products, add to cart, search etc.
--- src/components/data.jsx
+++ src/components/data.jsx
@@ -13,25 +13,29 @@ const products = [
         name: 'Grace',
         rating: 5,
         title: 'Excellent tea',
-        description: 'Great tea! One of the best I have found'
+        description: 'Great tea! One of the best I have found',
+        date: '12/08/2024'
       },
       {
         name: 'John',
         rating: 4,
         title: 'Very good',
-        description: 'Really good flavor, but I wish it was a bit stronger.'
+        description: 'Really good flavor, but I wish it was a bit stronger.',
+        date: '09/08/2024'
       },
       {
         name: 'Alice',
         rating: 5,
         title: 'My favorite!',
-        description: 'This tea has become my daily go-to. Highly recommend!'
+        description: 'This tea has become my daily go-to. Highly recommend!',
+        date: '15/06/2024'
       },
       {
         name: 'Alan',
         rating: 3.5,
         title: 'Nice tea',
-        description: 'Pleasant green tea, good quality, will buy again.'
+        description: 'Pleasant green tea, good quality, will buy again.',
+        date: '09/01/2024'
       }
     ]
   }
