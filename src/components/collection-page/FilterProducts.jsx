f0b17f12cb16b62690988d238b6313c2e96af791
Luke <lukedesmondjames@gmail.com>
2024-11-16 13:47:47 +0000

    function documentation complete

--- src/components/collection-page/FilterProducts.jsx
+++ src/components/collection-page/FilterProducts.jsx
@@ -3,20 +3,36 @@
 import { useEffect, useRef, useState } from 'react';
 import styles from './collectionPage.module.scss';
 
+/**
+ * Provides sorting options for a collection of products.
+ * Users can sort products by price (ascending or descending) or in alphabetical order.
+ *
+ * @component
+ * @param {Object} props - Component props
+ * @returns {JSX.Element} FilterProducts component
+ */
 const FilterProducts = ({ onSort }) => {
   const [isSortMenuVisible, setIsSortMenuVisible] = useState(false);
   const sortMenuRef = useRef(null);
 
+  /**
+   * Toggles visibility of sort menu dropdown.
+   */
   const handleSortClick = () => {
     setIsSortMenuVisible(!isSortMenuVisible);
   }
 
+  /**
+   * Closes sort menu if the user clicks outside of it.
+   * @param {Event} event - Click event
+   */
   const handleClickOutside = (event) => {
     if (sortMenuRef.current && !sortMenuRef.current.contains(event.target)) {
       setIsSortMenuVisible(false);
     }
   };
 
+  // Effect to listen for clicks outside the sort menu when it is open.
   useEffect(() => {
     if (isSortMenuVisible) {
       document.addEventListener('mousedown', handleClickOutside);
