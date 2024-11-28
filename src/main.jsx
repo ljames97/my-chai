39f1e3f8281ad683a19e9035e244323912ffda91
Luke <lukedesmondjames@gmail.com>
2024-11-28 13:38:30 +0000

    fixed error in BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/your-repository-name">
+    <BrowserRouter basename="/my-chai">
       <CartProvider>
         <ThemeProvider>
           <App />
